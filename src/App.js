ed9fdf8bcbc0086386bac4635d5be2b772cca5cf
DevPed <pedromfr91@gmail.com>
2022-03-23 10:12:57 +0100

    csstrick

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import GameArea from './containers/GameArea';
 
 function App() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
   return (
     <div className='App'>
       <GameArea />
